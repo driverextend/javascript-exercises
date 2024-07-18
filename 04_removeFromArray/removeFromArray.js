c0068af2ac150b4dd689cb2adb954c22b397004d
Blane Herndon <144256091+driverextend@users.noreply.github.com>
2024-07-18 16:20:50 -0500

    Complete lesson

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+
+    for (const ele of elements) {
+        while (arr.indexOf(ele) != -1) {
+            arr.splice(arr.indexOf(ele), 1)
+        }
+    }
+
+    return arr
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 1, 2)); // should remove 3 and return [1,2,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
