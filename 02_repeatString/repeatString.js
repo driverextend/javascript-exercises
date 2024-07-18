f1c94785e86094e9fd2960dd7f232f9146a5339e
Blane Herndon <144256091+driverextend@users.noreply.github.com>
2024-07-18 15:50:29 -0500

    Complete lesson

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
 
+    let repeatedString = "";
+
+    for(let i = 1; i <= num; i++){
+        repeatedString += string;
+    }
+
+    return repeatedString;
 };
+
+// repeatString('hey', 3) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
