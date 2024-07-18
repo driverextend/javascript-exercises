e3c51df12f95d914cdb528628f1dad8651fb2309
Blane Herndon <144256091+driverextend@users.noreply.github.com>
2024-07-18 16:01:46 -0500

    Complete lesson

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    wordExpanded = []
 
+    for (let index = word.length - 1; index >= 0; index--) {
+        wordExpanded.push(word[index])
+    }
+
+    return wordExpanded.join('')
 };
 
 // Do not edit below this line
