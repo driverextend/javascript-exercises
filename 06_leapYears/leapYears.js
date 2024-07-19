c4a001bac00fd81f79beb97522a9bf4d93ad4a84
Blane Herndon <144256091+driverextend@users.noreply.github.com>
2024-07-19 16:51:03 -0500

    Complete lesson

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0)
+            return (year % 400 == 0);
+        else return true
+    } else return false
 
+    return (year % 4 == 0 && year % 400 == 0)
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
+
 
 // Do not edit below this line
 module.exports = leapYears;
