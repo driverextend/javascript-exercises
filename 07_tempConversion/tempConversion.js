4a13cdf3a590b7ed59d651a0f8f266f264efe732
Blane Herndon <144256091+driverextend@users.noreply.github.com>
2024-07-19 17:06:11 -0500

    Complete lesson

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numInF) {
+  let numToC = (parseFloat(numInF) - 32) * 5 / 9
+  
+  return Math.round(numToC * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(numInC) {
+  let numToF = (parseFloat(numInC) * 9 / 5) + 32
+
+  return Math.round(numToF * 10) / 10
 };
+
+
+console.log(convertToCelsius(32)) // fahrenheit to celsius, should return 0
+console.log(convertToFahrenheit(0)) // celsius to fahrenheit, should return 32
 
 // Do not edit below this line
 module.exports = {
