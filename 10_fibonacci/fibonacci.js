6f36619a6962385e44b22f18089316b2a7bf5182
Blane Herndon <144256091+driverextend@users.noreply.github.com>
2024-08-03 20:45:48 -0500

    Complete lesson

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS"
+    if (n <= 0) return 0
+    if (n == 2 || n == 1) return 1
+    
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 };
+
+// console.log(fibonacci(5)); // 5
+// console.log(fibonacci(10)); // 55
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
