86571604c7bfdd1d5b601d24be5b9342d87e3557
Blane Herndon <144256091+driverextend@users.noreply.github.com>
2024-08-03 20:36:55 -0500

    Complete lesson

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let letters = word.toLowerCase().split("").filter(letter => letter !== "," && letter !== " " && letter !== "." && letter !== "!" )
+    // console.log(letters);
+    
+    for (let i = 0; i < letters.length / 2; i++) {        
+        if (letters[i] !== letters[letters.length - 1 - i]){
+            return false
+        }
+    }
+        
+    return true
 };
+
+
+// console.log(palindromes("racecar")); // true
+// console.log(palindromes("hello")); // false
+// console.log(palindromes("A car, a man, a maraca.")); // false
 
 // Do not edit below this line
 module.exports = palindromes;
